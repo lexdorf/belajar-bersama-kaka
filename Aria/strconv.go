63ed11d2b4f883c180e812ef6d3742b3810dbb35
Dimas Adi Andrea <dimas@jeneusindonesia.com>
2021-06-11 14:58:00 +0700

    saya menambahkan file baru

---
+++ Aria/strconv.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main(){
+	boolean, err := strconv.ParseBool("true")
+	if err == nil {
+		fmt.Println(boolean)
+	} else {
+		fmt.Println(err.Error())
+	}
+
+	number, err := strconv.ParseInt("1000000", 10, 64)
+	if err == nil {
+		fmt.Println(number)
+	} else {
+		fmt.Println(err.Error())
+	}
+
+	value := strconv.FormatInt(1000000, 10)
+	value1 := strconv.FormatInt(1000000, 2)
+	value2 := strconv.FormatInt(1000000, 8)
+	value3 := strconv.FormatInt(1000000, 16)
+	fmt.Println(value)
+	fmt.Println(value1)
+	fmt.Println(value2)
+	fmt.Println(value3)
+
+	valueInt, _ := strconv.Atoi("2000000")
+	fmt.Println(valueInt)
+}
