5d0492595384a1bffd9626da29d2b2c8084f0580
Dimas Adi Andrea <dimas@jeneusindonesia.com>
2021-06-21 13:33:35 +0700

    Saya baru saja menambahkan file

--- Aria/belajar-golang-goroutines/channel_test.go
+++ Aria/belajar-golang-goroutines/channel_test.go
@@ -96,3 +96,71 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("Selesai")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+
+//	select {
+//	case data := <-channel1:
+//		fmt.Println("Data dari Channel 1", data)
+//	case data := <-channel2:
+//		fmt.Println("Data dari Channel 2", data)
+//	}
+
+//	select {
+//	case data := <-channel1:
+//		fmt.Println("Data dari Channel 1", data)
+//	case data := <-channel2:
+//		fmt.Println("Data dari Channel 2", data)
+//	}
+}
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
