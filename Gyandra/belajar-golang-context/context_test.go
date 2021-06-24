9c0b321a1aec8f8dda25663a383b919bed6e925d
Gyandra Naupal <gyandranaupal14@gmail.com>
2021-06-24 11:20:48 +0700

    saya baru saja menambahkan file baru

---
+++ Gyandra/belajar-golang-context/context_test.go
@@ -0,0 +1,15 @@
+package belajar_golang_context
+
+import (
+	"fmt"
+	"context"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
